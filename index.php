7eea2bbec90960222ca3af8909e9c0e746faa8ff
AriSyafri <diamondThink15@gmail.com>
2023-08-03 20:36:56 +0700

    menambah navbar

--- index.php
+++ index.php
@@ -10,7 +10,29 @@
 </head>
 
 <body>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Pascal Scanner</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="d-flex" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Scanner</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="code.php">Code</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- navbar penutup -->
+
     <div class="container">
+
         <div class="d-flex justify-content-center">
             <h1>Pascal Scanner Output</h1>
         </div>
